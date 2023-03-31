edc6884fedd5239a57e8797d6a3ddc551b494fae
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-31 20:53:38 +0200

    FEAT create constructor and add parent constructor

---
+++ models/Food.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . "/Product.php";
+
+
+class Food extends Product
+{
+    public $hasMeet = true;
+    public string $consistency;
+
+    function __construct($_hasMeet, $_consistency, $_quantity, $_title, $_price, $_description, $_img)
+    {
+        parent::__construct($_quantity, $_title, $_price, $_description, $_img);
+        $this->hasMeet = $_hasMeet;
+        $this->consistency = $_consistency;
+    }
+    function checkMeet()
+    {
+    }
+}
