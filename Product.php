fc43f43b75f4383941690baee8b0129a1d2267d8
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-31 15:56:57 +0200

    FEAT create class product

---
+++ Product.php
@@ -0,0 +1,12 @@
+<?php
+
+//per prima cosa creo la classe principale di prodotti
+
+class Products{
+    public int $quantity;
+    public bool $presence;
+    public string $title;
+    public string $description;
+    
+    
+}
