65ee2f507b93472d1e6743cb2f0573c2690b1d2a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-31 20:54:23 +0200

    FEAT product form and begin check function

---
+++ models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+//per prima cosa creo la classe principale di prodotti
+
+class Product
+{
+
+    public int $quantity;
+    public string $title;
+    public string $description;
+    public float $price;
+    public string $img;
+
+
+
+
+    function __construct($_quantity, $_title, $_price, $_description, $_img)
+    {
+        $this->quantity = $_quantity;
+        $this->title = $_title;
+        $this->price = $_price;
+        $this->description = $_description;
+        $this->img = $_img;
+    }
+    // public function setQuantity($value){
+    //     if($value < 0){
+    //         return "Prodotto non disponibile";
+    //     } else if (is_null($value) || is_nan($value)){
+    //         return "Nessun numero valido inserito";
+    //     }
+    //     return $this->quantity = $value;
+
+    // }
+}
