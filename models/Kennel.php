89f368f4f4dc28fda629459e1c33b8e855fe8d10
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-01 09:23:19 +0200

    FEAT create kennel constructor and link with prod

--- models/Kennel.php
+++ models/Kennel.php
@@ -1,7 +1,15 @@
 <?php
-
+require_once __DIR__ . "/Product.php";
 class Kennel extends Product
 {
     public string $tissue;
     public string $shape;
+
+
+    function __construct($_tissue, $_shape, $_quantity, $_title, $_price, $_description, $_img)
+    {
+        parent::__construct($_quantity, $_title, $_price, $_description, $_img);
+        $this->tissue = $_tissue;
+        $this->shape = $_shape;
+    }
 }
